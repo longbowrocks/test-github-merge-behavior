b2fc6b0d0bf553e564bacdf02fbe27ac7a0459d2
cmercer <longbowmail@yahoo.com>
2023-01-16 15:42:08 -0800

    Followup version-specific bugfix.

--- source_code.py
+++ source_code.py
@@ -7,6 +7,7 @@ def generic_code():
 
 def version_specific_code():
     print("I am version 2")
+    print("Version 2 requires a custom SSL certfile to make its API calls consistent with other versions."
 
 
 def main():
