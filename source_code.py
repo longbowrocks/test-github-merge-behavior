82261eb870f9c6f5becda3d700ac9b42870fc292
cmercer <longbowmail@yahoo.com>
2023-01-16 15:15:06 -0800

    Commit version 1 of the project

---
+++ source_code.py
@@ -0,0 +1,17 @@
+
+
+def generic_code():
+    print("Hello World!")
+
+
+def version_specific_code():
+    print("I am version 1")
+
+
+def main():
+    generic_code()
+    version_specific_code()
+
+
+if __name__ == "__main__":
+    main()
